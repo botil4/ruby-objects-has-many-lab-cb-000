1d5c87c5f191e2b7a32c7d93f1bc003efd911963
Travis Clarkson <tk1667@gmail.com>
2018-02-24 22:50:49 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,9 +2,17 @@ class Author
 
  attr_accessor :name, :posts
 
+ @@post_count
+
  def initialize(name)
    @name = name
    @posts = []
  end
+
+ def add_post(post)
+   @posts << post
+   post.author = self
+   @@post_count += 1
+ end 
 
 end
